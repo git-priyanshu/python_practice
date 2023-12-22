31f1b02ffa92e078a502fba38759c81aa05be12f
singh7970 <priy2032004anshu@gmail.com>
2023-12-22 16:27:07 +0530

    random_guess

---
+++ 2_Randomnum.py
@@ -0,0 +1,14 @@
+import random 
+comp=random.randint(1,10)
+user=int(input("Enter the number"))
+print("computer number",comp)
+print("user number",user)
+
+if (user > comp):
+    print(f"{user} is greter then{ comp}")
+elif (user <comp):
+    print(f"{user} is less then {comp}")  
+elif (user==comp):
+    print("Your number is equal to computer number")      
+else:
+    print("retry")    
